8cdfcb2dfa04f62d39089615b1cf2af72a17e9de
Pablo Gonzalez Ruiz <juagon@microsoft.com>
2025-04-07 15:25:51 -0700

    Adding first files

---
+++ src/Reservoom/App.xaml.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Reservoom.DbContexts;
+using Reservoom.Exceptions;
+using Reservoom.HostBuilders;
+using Reservoom.Models;
+using Reservoom.Services;
+using Reservoom.Services.ReservationConflictValidators;
+using Reservoom.Services.ReservationCreators;
+using Reservoom.Services.ReservationProviders;
+using Reservoom.Stores;
+using Reservoom.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Reservoom
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .AddViewModels()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    bool isEndToEndTest = Environment.GetCommandLineArgs().Any(a => a == "E2E");
+
+                    if (!isEndToEndTest)
+                    {
+                        string connectionString = hostContext.Configuration.GetConnectionString("Default");
+                        services.AddSingleton<IReservoomDbContextFactory>(new ReservoomDbContextFactory(connectionString));
+                    } 
+                    else
+                    {
+                        services.AddSingleton<IReservoomDbContextFactory>(new InMemoryReservoomDbContextFactory());
+                    }
+
+                    services.AddSingleton<IReservationProvider, DatabaseReservationProvider>();
+                    services.AddSingleton<IReservationCreator, DatabaseReservationCreator>();
+                    services.AddSingleton<IReservationConflictValidator, DatabaseReservationConflictValidator>();
+
+                    services.AddTransient<ReservationBook>();
+
+                    string hotelName = hostContext.Configuration.GetValue<string>("HotelName");
+                    services.AddSingleton((s) => new Hotel(hotelName, s.GetRequiredService<ReservationBook>()));
+
+                    services.AddSingleton<HotelStore>();
+                    services.AddSingleton<NavigationStore>();
+
+                    services.AddSingleton(s => new MainWindow()
+                    {
+                        DataContext = s.GetRequiredService<MainViewModel>()
+                    });
+                })
+                .Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+
+            IReservoomDbContextFactory reservoomDbContextFactory = _host.Services.GetRequiredService<IReservoomDbContextFactory>();
+            using (ReservoomDbContext dbContext = reservoomDbContextFactory.CreateDbContext())
+            {
+                dbContext.Database.Migrate();
+            }
+
+            NavigationService<ReservationListingViewModel> navigationService = _host.Services.GetRequiredService<NavigationService<ReservationListingViewModel>>();
+            navigationService.Navigate();
+
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host.Dispose();
+
+            base.OnExit(e);
+        }
+    }
+}
---
+++ src/Reservoom/DbContexts/ReservoomDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom.DbContexts
+{
+    public class ReservoomDbContextFactory : IReservoomDbContextFactory
+    {
+        private readonly string _connectionString;
+
+        public ReservoomDbContextFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public ReservoomDbContext CreateDbContext()
+        {
+            DbContextOptions options = new DbContextOptionsBuilder().UseSqlite(_connectionString).Options;
+
+            return new ReservoomDbContext(options);
+        }
+    }
+}
