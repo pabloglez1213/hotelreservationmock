98188ef64e1b8af58fbdf3ca7c7e952c28b9684b
Pablo Gonzalez Ruiz <juagon@microsoft.com>
2025-10-30 10:59:22 -0700

    changing path

---
+++ src/NestedProject/NestedProject/nestedlevel1/nestedlevel2/nestedlevel4/nested.level5/Pets.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedProject.nestedlevel1.nestedlevel2.nestedlevel4.nestedlevel5
+{
+    internal class Pets
+    {
+    }
+}
